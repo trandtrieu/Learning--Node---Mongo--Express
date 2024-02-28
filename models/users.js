32fddb4abc950e2f46e7d87187ee57ace0ecbd97
trandtrieu <114125026+trandtrieu@users.noreply.github.com>
2024-02-28 14:58:38 +0700

    jwt

--- models/users.js
+++ models/users.js
@@ -3,6 +3,16 @@ var Schema = mongoose.Schema;
 var passportLocalMongoose = require("passport-local-mongoose");
 
 var User = new Schema({
+  // username: {
+  //   type: String,
+  //   required: true,
+  //   unique: true,
+  // },
+  // password: {
+  //   type: String,
+  //   required: true,
+  // },
+
   admin: {
     type: Boolean,
     default: false,
