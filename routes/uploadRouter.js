659c2ba400d063ab23fa9f16e9ac909532c6e486
trandtrieu <114125026+trandtrieu@users.noreply.github.com>
2024-02-29 14:00:28 +0700

    upload file

--- routes/uploadRouter.js
+++ routes/uploadRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const authenticate = require("../authenticate");
+const authenticate = require("../models/authenticate");
 const multer = require("multer");
 
 const storage = multer.diskStorage({
